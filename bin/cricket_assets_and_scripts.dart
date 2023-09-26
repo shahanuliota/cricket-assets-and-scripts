256a63fdb7b3b41a647b66eeb69cb367def1462e
shahanuliota <shahanul.iotait@gmail.com>
2023-09-26 12:56:44 +0600

    Initial commit

---
+++ bin/cricket_assets_and_scripts.dart
@@ -0,0 +1,5 @@
+import 'package:cricket_assets_and_scripts/cricket_assets_and_scripts.dart' as cricket_assets_and_scripts;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cricket_assets_and_scripts.calculate()}!');
+}
---
+++ test/cricket_assets_and_scripts_test.dart
@@ -0,0 +1,8 @@
+import 'package:cricket_assets_and_scripts/cricket_assets_and_scripts.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
